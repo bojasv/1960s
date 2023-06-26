b84d0af28f6dfc07e22d8790687bd04629271c8c
bojasv <59419054+bojasv@users.noreply.github.com>
2023-06-26 19:55:27 +0530

    Update QRPGLESRC.sql

--- IBMi_SQL/QRPGLESRC.sql
+++ IBMi_SQL/QRPGLESRC.sql
@@ -1,4 +1,5 @@
-create table qtemp.qsqlsrc (srcseq num(6,2), srcdat num(6,0), srcdta char(100));
+drop table qtemp.qsqlsrc;
+create table qtemp.qsqlsrc (srcseq num(6,2), srcdat num(6,0), srcdta char(100)) rcdfmt testfa;
 insert into qtemp.qsqlsrc (srcdta) values('    create  or   replace   procedure'),( '    create '), ('    or '), ('  replace '), ('     procedure ');
 select * from qtemp.qsqlsrc;
 
