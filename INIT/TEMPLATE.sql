d6b4c468dc139e671bea84414b8116c69cb0751a
bojasv <59419054+bojasv@users.noreply.github.com>
2023-08-07 20:42:30 +0530

    Create TEMPLATE.sql

---
+++ INIT/TEMPLATE.sql
@@ -0,0 +1,4 @@
+INSERT INTO OJASVA.QCLSRC VALUES(000010, 000000, '             pgm') ;
+INSERT INTO OJASVA.QCLSRC VALUES(000010, 000000, '             dcl &var_string *char 5000 ''hello world''') ;
+INSERT INTO OJASVA.QCLSRC VALUES(000010, 000000, '             dcl &var_packed *dec (15 9)') ;
+INSERT INTO OJASVA.QCLSRC VALUES(000010, 000000, ' endthepgmx: endpgm') ; 
