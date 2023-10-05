21a5e8192b964cacd13d19ed45e952b0f6393b33
bojasv <59419054+bojasv@users.noreply.github.com>
2023-10-05 20:54:09 +0530

    Update BestPractices.sql

--- IBMi_SQL/BestPractices.sql
+++ IBMi_SQL/BestPractices.sql
@@ -17,3 +17,19 @@ Use SQLSTATE over SQLCODE
 Use SQLER3, over GET DIAGNOSTICS  
 SELECT Column0 FROM Table1 (INSERT INTO Table0 (Column0, Column1) VALUES (NEXT VALUES FOR Column0, 'Test Data'));
 Use of MERGE, rather SELECT, 'IF-Else-EndIf' block, INSERT and UPDATE
+
+-- Check Current Number of Records
+SELECT COUNT(*) FROM TSTLIB.TSTFILE;
+-- Check If any deleted record. If DELETED_RCDS > 0, i.e. RRN can't be same as Sequence#
+SELECT (MAX(RRN(a))-COUNT(*)) AS DELETED_RCDS FROM TSTLIB.TSTFILE a;
+-- Look at the recent 10 records
+-- Look at the Column Names, Type and Size
+SELECT * FROM TSTLIB.TSTFILE a WHERE RRN(a) > (SELECT MAX(RRN(a))-11 FROM TSTLIB.TSTFILE a);
+-- Look for any NULL column (it may require NULL handling)
+SELECT COUNT(*) FROM TSTLIB.TSTFILE WHERE COL1 IS NULL OR COL2 IS NULL;
+-- Look for duplicate key records, if present order them by the frequence of their duplicacy. Say COL1 is the key-field 
+SELECT COL1, COUNT(*) AS Freq FROM TSTLIB.TSTFILE GROUP BY COL1 HAVING COUNT(*)  > 1 ORDER BY Freq DESC;
+-- Look for the earliest dated record
+SELECT MIN(COL_DATE) FROM TSTLIB.TSTFILE;
+-- Look for the latest date record
+SELECT MAX(COL_DATE) FROM TSTLIB.TSTFILE;
